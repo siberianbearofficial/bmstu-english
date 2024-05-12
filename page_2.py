1b4c802fcf503c99ebf3d1bb18dfb4534d04c77b
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-05-13 00:31:20 +0300

    page2 refactoring

--- page_2.py
+++ page_2.py
@@ -1,39 +1,45 @@
 from bs4 import BeautifulSoup
 
 
-def only_underlinings(w):
-    for c in w:
-        if c != '_':
-            return False
-    return True
+def extract_task_text(elements):
+    task_text = list()
+    for element in elements:
+        if element.text.strip():
+            task_text.append(element.text.strip())
+    return task_text
 
 
-def main():
-    def format_blank(words, ind):
-        blank = blanks_options[ind]
-        return blank if only_underlinings(words[-1]) else f'{blank} {words[-1].strip("_")}'
+def extract_blank_options(elements):
+    blank_options = list()
+    for option in elements:
+        if option.text.strip():
+            blank_options.append(option.text.strip())
+    return blank_options
 
-    with open(input('Введите путь к файлу второй страницы: ').strip(), 'r', encoding='utf-8') as file:
-        html_content = file.read()
 
-    soup = BeautifulSoup(html_content, 'lxml')
+def extract_task_text_and_options(content):
+    soup = BeautifulSoup(content, 'lxml')
     soup = soup.select('#page-content')[0].select('.content')
 
     task_text = list()
     blanks_options = list()
     ind = 0
     for el in soup:
-        for p_element in el.find_all('p'):
-            if p_element.text.strip():
-                task_text.append(p_element.text.strip())
+        task_text.extend(extract_task_text(el.find_all('p')))
         for select_element in el.find_all('select'):
-            blank_options = list()
-            for option in select_element.find_all('option'):
-                if option.text.strip():
-                    blank_options.append(option.text.strip())
+            blank_options = extract_blank_options(select_element.find_all('option'))
             blanks_options.append(f'[{ind + 1}: {", ".join(blank_options)}]')
             ind += 1
 
+    return task_text, blanks_options
+
+
+def format_blank(words, ind, blanks_options):
+    blank = blanks_options[ind]
+    return blank if all(c == '_' for c in words[-1]) else f'{blank} {words[-1].strip("_")}'
+
+
+def pack_task_text_with_options(task_text, blanks_options):
     blank_index = 0
     new_task_text = list()
     for par in task_text:
@@ -50,12 +56,21 @@ def main():
             elif blank_started and '__' in word:
                 in_blank.append(word)
                 blank_started = False
-                new_words.append(format_blank(in_blank, blank_index))
+                new_words.append(format_blank(in_blank, blank_index, blanks_options))
                 blank_index += 1
                 in_blank = list()
             else:
                 new_words.append(word)
         new_task_text.append(' '.join(new_words))
+    return new_task_text
+
+
+def main():
+    with open(input('Введите путь к файлу второй страницы: ').strip(), 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    task_text, options = extract_task_text_and_options(content)
+    new_task_text = pack_task_text_with_options(task_text, options)
 
     print('Choose the best word to fill the gap. Write the answers each on new line in the format: n. chosen_word, '
           'where n is the number of the blank and chosen_word is your choice. NOTHING ELSE should be in the answer!')
