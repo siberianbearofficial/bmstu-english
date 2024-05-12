949ab5e2aa62d0426ea51051b275eb9405fe1967
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-05-12 23:18:32 +0300

    page 1 refactoring

--- page_1.py
+++ page_1.py
@@ -1,62 +1,83 @@
 from bs4 import BeautifulSoup
 
 
-def only_underlinings(w):
-    for c in w:
-        if c != '_':
-            return False
-    return True
+def split_question_and_underlinings(w):
+    w = w.strip()
+    underlinings = True
+    start_underlinings = True
+    start_count = end_count = 0
+
+    for el in w:
+        if el == '_' and underlinings and start_underlinings:
+            start_count += 1
+        elif el == '_' and underlinings and not start_underlinings:
+            end_count += 1
+        elif el != '_':
+            start_underlinings = False
+
+    new_q = list()
+    if start_count:
+        new_q.append(start_count * '_')
+    new_q.append(w.strip('_'))
+    if end_count:
+        new_q.append(end_count * '_')
+    return new_q
 
 
-def main():
-    with open(input('Введите путь к файлу первой страницы: ').strip(), 'r', encoding='utf-8') as file:
-        html_content = file.read()
+def pack_questions_with_answers(questions, answers):
+    lines = list()
+    for i in range(len(questions)):
+        new_question = list()
+        for w in questions[i].strip().split():
+            if '_' in questions[i] and not all(c == '_' for c in w):
+                new_question.extend(split_question_and_underlinings(w))
+            else:
+                new_question.append(w.strip())
+        lines.append(f'{i + 1}. {" ".join(new_question)}')
+        lines.append(answers[i])
+    return lines
 
-    soup = BeautifulSoup(html_content, 'lxml')
-    soup = soup.select('#page-content')[0].select('.content')
+
+def extract_questions(elements):
+    questions = list()
+    for element in elements:
+        if element.text.strip():
+            questions.append(element.text.strip())
+    return questions
+
+
+def extract_answers(elements):
+    answer = list()
+    for element in elements:
+        if element.text.strip():
+            for a in element.text.strip().split('\n'):
+                answer.append(a.strip())
+    return '; '.join(answer)
+
+
+def extract_questions_and_answers(content):
+    soup = BeautifulSoup(content, 'lxml').select('#page-content')[0].select('.content')
 
     questions = list()
     answers = list()
 
-    for el in soup:
-        for element in el.select('.qtext'):
-            if element.text.strip():
-                questions.append(element.text.strip())
-        answer = list()
-        for element in el.select('.answer'):
-            if element.text.strip():
-                for a in element.text.strip().split('\n'):
-                    answer.append(a.strip())
-        answers.append('; '.join(answer))
+    for element in soup:
+        questions.extend(extract_questions(element.select('.qtext')))
+        answers.append(extract_answers(element.select('.answer')))
+
+    return questions, answers
+
+
+def main():
+    with open(input('Введите путь к файлу первой страницы: ').strip(), 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    questions, answers = extract_questions_and_answers(content)
 
     print('Choose a, b or c to fill the gaps. Write the answers each on new line in the format: n) letter. answer, '
           'where n is the question number, letter is a, b or c and answer is the associated with the letter answer that'
           'fits the gap best. NOTHING ELSE should be in the answer!')
-    for i in range(len(questions)):
-        q = questions[i]
-        new_q = list()
-        for w in q.strip().split():
-            if '_' in q and not only_underlinings(w):
-                w = w.strip()
-                underlinings = True
-                start_underlinings = True
-                start_count = end_count = 0
-                for el in w:
-                    if el == '_' and underlinings and start_underlinings:
-                        start_count += 1
-                    elif el == '_' and underlinings and not start_underlinings:
-                        end_count += 1
-                    elif el != '_':
-                        start_underlinings = False
-                if start_count:
-                    new_q.append(start_count * '_')
-                new_q.append(w.strip('_'))
-                if end_count:
-                    new_q.append(end_count * '_')
-            else:
-                new_q.append(w.strip())
-        print(i + 1, ' '.join(new_q))
-        print(answers[i])
+    print(*pack_questions_with_answers(questions, answers), sep='\n')
 
 
 if __name__ == '__main__':
